c57b31e089be486ade5754f8bbedf63466855f4a
adamg <adamg88@users.noreply.github.com>
2019-06-06 02:30:15 +0100

    Prompt user if they would like to update if updates set to manual

--- src/resources/lib/modules/updates.py
+++ src/resources/lib/modules/updates.py
@@ -567,6 +567,12 @@ class updates:
                     if self.struct['update']['settings']['AutoUpdate']['value'] == 'auto' and force == False:
                         self.update_in_progress = True
                         self.do_autoupdate(None, True)
+                    else:
+                        if self.oe.BUILD == 'official':
+                            ceUpdate = xbmcgui.Dialog().yesno('CoreELEC', 'An update is available, would you like to download it now?')
+                            if(ceUpdate):
+                                self.update_in_progress = True
+                                self.do_autoupdate(None, True)
             self.oe.dbg_log('updates::check_updates_v2', 'exit_function', 0)
         except Exception, e:
             self.oe.dbg_log('updates::check_updates_v2', 'ERROR: (' + repr(e) + ')')
