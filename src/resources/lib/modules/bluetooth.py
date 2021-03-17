8087c249ec31addea68c983e6097555dcb8eb405
Portisch <hugo.portisch@yahoo.de>
2021-03-17 14:03:08 +0100

    bluetooth: ignore interfaces with no 'Device' or 'Class' property

--- src/resources/lib/modules/bluetooth.py
+++ src/resources/lib/modules/bluetooth.py
@@ -12,6 +12,7 @@ import dbus
 import threading
 import oeWindows
 
+from dbus import DBusException
 
 class bluetooth:
 
@@ -818,13 +819,17 @@ class bluetooth:
                     if 'org.bluez.' in interface:
                         device = self.oe.dbusSystemBus.get_object('org.bluez', path)
                         device_iface = dbus.Interface(device, dbus.PROPERTIES_IFACE)
-                        device_path = device_iface.Get(interface, 'Device')
 
-                        device_path = self.oe.dbusSystemBus.get_object('org.bluez', device_path)
-                        device = dbus.Interface(device_path, dbus.PROPERTIES_IFACE)
+                        try:
+                            device_path = device_iface.Get(interface, 'Device')
+                            device_path = self.oe.dbusSystemBus.get_object('org.bluez', device_path)
+                            device = dbus.Interface(device_path, dbus.PROPERTIES_IFACE)
 
-                        Address = str(device.Get('org.bluez.Device1', 'Address'))
-                        Class = int(device.Get('org.bluez.Device1', 'Class'))
+                            Address = str(device.Get('org.bluez.Device1', 'Address'))
+                            Class = int(device.Get('org.bluez.Device1', 'Class'))
+                        except DBusException, e:
+                            self.oe.dbg_log('bluetooth::monitor::InterfacesAdded', 'WARNING: (' + repr(e) + ')', 0)
+                            continue
 
                         if not Address in self.devices:
                             self.devices[Address] = {}
